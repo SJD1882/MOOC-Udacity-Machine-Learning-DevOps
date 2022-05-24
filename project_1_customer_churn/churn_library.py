c26e2b75fcfa575da0f952b4ce1b81df80680273
SJD1882 <sjd1882@gmail.com>
2022-05-24 16:55:05 +0200

    Resend pylint compliant scripts

--- project_1_customer_churn/churn_library.py
+++ project_1_customer_churn/churn_library.py
@@ -1,12 +1,15 @@
-# Author - Sebastien
-# Date - 2022/05/24
-# Reads customer churn data, performs exploratory data analysis and
-# trains models for predicting likelihood of customer churn
+"""
+Author - Sebastien
+Date - 2022/05/24
+
+Reads customer churn data, performs exploratory data analysis and
+trains models for predicting likelihood of customer churn
+"""
 
 # Packages
 import sys
-import joblib
 import logging
+import joblib
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -34,6 +37,8 @@ file_logger.setFormatter(file_format)
 logger.addHandler(file_logger)
 
 # Functions
+
+
 def import_data(pth: str) -> pd.DataFrame:
     '''
     returns dataframe for the csv found at pth
@@ -45,17 +50,17 @@ def import_data(pth: str) -> pd.DataFrame:
     '''
     try:
         # Check if file path is of CSV format
-        assert(pth.rsplit('.', 1)[-1] == 'csv')
-        
+        assert pth.rsplit('.', 1)[-1] == 'csv'
+
         # Read dataset
         data = pd.read_csv(pth, index_col=0)
-        
+
         # Run some basic preprocessing
         data['Churn'] = data['Attrition_Flag'].apply(
-                lambda val: 0 if val == "Existing Customer" else 1
-                )
+            lambda val: 0 if val == "Existing Customer" else 1
+        )
         logger.info('Imported Dataset')
-        
+
     except FileNotFoundError:
         logger.error(f'Path provided {pth} either does not exist.')
         raise FileNotFoundError
@@ -63,8 +68,8 @@ def import_data(pth: str) -> pd.DataFrame:
     except AssertionError:
         logger.error(f'Path provided {pth} is not of CSV format.')
         raise AssertionError
-    
-    return data 
+
+    return data
 
 
 def perform_eda(df: pd.DataFrame, output_path: str) -> None:
@@ -78,16 +83,16 @@ def perform_eda(df: pd.DataFrame, output_path: str) -> None:
             None
     '''
     # Plot Customer Churn Distribution
-    _, ax = plt.subplots(figsize=(8,5))
+    _, ax = plt.subplots(figsize=(8, 5))
     df['Churn'].hist(ax=ax, color='blue')
     ax.set_title('Distribution of customer churn', fontsize=12)
     ax.set_xlabel('0 = Existing Customer; 1 = Customer Churn', fontsize=12)
     ax.set_ylabel('Distribution', fontsize=12)
     plt.savefig(f'{output_path}/churn_distribution.png', bbox_inches='tight')
     plt.close()
-    
+
     # Plot Customer Age Distribution
-    _, ax = plt.subplots(figsize=(8,5))
+    _, ax = plt.subplots(figsize=(8, 5))
     df['Customer_Age'].hist(ax=ax, color='blue',
                             edgecolor='white')
     ax.set_title('Distribution of customer churn', fontsize=12)
@@ -95,11 +100,11 @@ def perform_eda(df: pd.DataFrame, output_path: str) -> None:
     ax.set_ylabel('Distribution', fontsize=12)
     plt.savefig(f'{output_path}/age_distribution.png', bbox_inches='tight')
     plt.close()
-    
+
     # Plot Marital Status Distribution
-    _, ax = plt.subplots(figsize=(8,5))
+    _, ax = plt.subplots(figsize=(8, 5))
     df['Marital_Status'].value_counts(True) \
-            .plot(kind='bar', ax=ax, color='blue')
+        .plot(kind='bar', ax=ax, color='blue')
     ax.set_title('Distribution of marital status', fontsize=12)
     ax.set_xlabel('Marital Status', fontsize=12)
     ax.set_ylabel('Distribution', fontsize=12)
@@ -109,7 +114,7 @@ def perform_eda(df: pd.DataFrame, output_path: str) -> None:
     plt.close()
 
     # Plot Total Transaction Count
-    _, ax = plt.subplots(figsize=(8,5))
+    _, ax = plt.subplots(figsize=(8, 5))
     sns.histplot(df['Total_Trans_Ct'], ax=ax, color='blue',
                  kde=True)
     ax.set_title('Distribution of total transaction counts', fontsize=12)
@@ -118,16 +123,16 @@ def perform_eda(df: pd.DataFrame, output_path: str) -> None:
     plt.savefig(f'{output_path}/total_transaction_distribution.png',
                 bbox_inches='tight')
     plt.close()
-    
+
     # Plot Data Set Heatmap
-    _, ax = plt.subplots(figsize=(8,6))
+    _, ax = plt.subplots(figsize=(8, 6))
     sns.heatmap(df.corr(), annot=False, ax=ax, linewidths=2,
                 cmap='RdYlGn_r', vmin=-1.0, vmax=1.0)
     ax.set_title('Correlation map of customer churn dataset', fontsize=12)
     plt.savefig(f'{output_path}/correlation_map.png',
                 bbox_inches='tight')
     plt.close()
-    
+
     logger.info('Performed Exploratory Data Analysis')
 
 
@@ -142,30 +147,34 @@ def encoder_helper(df: pd.DataFrame,
     input:
             df: pandas dataframe
             category_lst: list of columns that contain categorical features
-            response: string of response name [optional argument that could be used for naming variables or index y column]
+            response: string of response name [optional argument that could be
+            used for naming variables or index y column]
 
     output:
             df: pandas dataframe with new columns for
     '''
-    data = df.copy() 
+    data = df.copy()
     for category_feature in category_lst:
-        
+
         # Dictionary containing average churn per category
         category_feature_dict = data.groupby(category_feature)[response].mean()
         category_feature_dict = category_feature_dict.to_dict()
-        
+
         # Map categories with their average churn percentage
         new_feature = f'{category_feature}_Churn'
         data[new_feature] = data[category_feature].map(category_feature_dict)
-        
+
     return data
 
 
-def perform_feature_engineering(df: pd.DataFrame, response: str) -> pd.DataFrame:
+def perform_feature_engineering(
+        df: pd.DataFrame,
+        response: str) -> pd.DataFrame:
     '''
     input:
               df: pandas dataframe
-              response: string of response name [optional argument that could be used for naming variables or index y column]
+              response: string of response name [optional argument that could be used
+              for naming variables or index y column]
 
     output:
               X_train: X training data
@@ -174,27 +183,40 @@ def perform_feature_engineering(df: pd.DataFrame, response: str) -> pd.DataFrame
               y_test: y testing data
     '''
     data = df.copy()
-    
+
     # Encoding Categorical Variables
     category_vars = ['Gender', 'Education_Level', 'Marital_Status',
                      'Income_Category', 'Card_Category']
     data_category_encoded = encoder_helper(data, category_vars,
-                                           response = 'Churn')
-    
+                                           response='Churn')
+
     # Keep Relevant Variables
-    keep_cols = ['Customer_Age', 'Dependent_count', 'Months_on_book',
-                 'Total_Relationship_Count', 'Months_Inactive_12_mon',
-                 'Contacts_Count_12_mon', 'Credit_Limit', 'Total_Revolving_Bal',
-                 'Avg_Open_To_Buy', 'Total_Amt_Chng_Q4_Q1', 'Total_Trans_Amt',
-                 'Total_Trans_Ct', 'Total_Ct_Chng_Q4_Q1', 'Avg_Utilization_Ratio',
-                 'Gender_Churn', 'Education_Level_Churn', 'Marital_Status_Churn', 
-                 'Income_Category_Churn', 'Card_Category_Churn']
+    keep_cols = [
+        'Customer_Age',
+        'Dependent_count',
+        'Months_on_book',
+        'Total_Relationship_Count',
+        'Months_Inactive_12_mon',
+        'Contacts_Count_12_mon',
+        'Credit_Limit',
+        'Total_Revolving_Bal',
+        'Avg_Open_To_Buy',
+        'Total_Amt_Chng_Q4_Q1',
+        'Total_Trans_Amt',
+        'Total_Trans_Ct',
+        'Total_Ct_Chng_Q4_Q1',
+        'Avg_Utilization_Ratio',
+        'Gender_Churn',
+        'Education_Level_Churn',
+        'Marital_Status_Churn',
+        'Income_Category_Churn',
+        'Card_Category_Churn']
 
     X = data_category_encoded[keep_cols].copy()
     y = data_category_encoded[response].copy()
-    
+
     # Training/Testing Split
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.3,
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3,
                                                         random_state=SEED)
     result = X_train, X_test, y_train, y_test
 
@@ -211,8 +233,8 @@ def classification_report_image(y_train: pd.DataFrame,
                                 y_test_preds_rf: pd.DataFrame,
                                 output_folder: str):
     '''
-    produces classification report for training and testing results and stores report as image
-    in images folder
+    produces classification report for training and testing results and stores report
+    as image in images folder
     input:
             y_train: training response values
             y_test:  test response values
@@ -230,35 +252,39 @@ def classification_report_image(y_train: pd.DataFrame,
     ytrain_preds_rf = (y_train_preds_rf >= 0.5).astype(int)
     ytest_preds_lr = (y_test_preds_lr >= 0.5).astype(int)
     ytest_preds_rf = (y_test_preds_rf >= 0.5).astype(int)
-    
+
     # Generate Random Forest Reports
     plt.rc('figure', figsize=(5, 5))
     plt.text(0.01, 1.25, str('Random Forest Train'),
-             {'fontsize': 10}, fontproperties = 'monospace')
+             {'fontsize': 10}, fontproperties='monospace')
     plt.text(0.01, 0.7, str(classification_report(y_train, ytrain_preds_rf)),
-             {'fontsize': 10}, fontproperties = 'monospace')
-    
+             {'fontsize': 10}, fontproperties='monospace')
+
     plt.text(0.01, 0.6, str('Random Forest Test'), {'fontsize': 10},
-             fontproperties = 'monospace')
+             fontproperties='monospace')
     plt.text(0.01, 0.05, str(classification_report(y_test, ytest_preds_rf)),
-             {'fontsize': 10}, fontproperties = 'monospace')
+             {'fontsize': 10}, fontproperties='monospace')
     plt.axis('off')
-    plt.savefig(f'{output_folder}/random_forest_classification_report.png', bbox_inches='tight')
+    plt.savefig(
+        f'{output_folder}/random_forest_classification_report.png',
+        bbox_inches='tight')
     plt.close()
-    
+
     # Generate Logistic Regression Reports as Images
     plt.rc('figure', figsize=(5, 5))
     plt.text(0.01, 1.25, str('Logistic Regression Train'),
-             {'fontsize': 10}, fontproperties = 'monospace')
+             {'fontsize': 10}, fontproperties='monospace')
     plt.text(0.01, 0.7, str(classification_report(y_train, ytrain_preds_lr)),
-             {'fontsize': 10}, fontproperties = 'monospace')
-    
+             {'fontsize': 10}, fontproperties='monospace')
+
     plt.text(0.01, 0.6, str('Logistic Regression Test'), {'fontsize': 10},
-             fontproperties = 'monospace')
+             fontproperties='monospace')
     plt.text(0.01, 0.05, str(classification_report(y_test, ytest_preds_lr)),
-             {'fontsize': 10}, fontproperties = 'monospace')
+             {'fontsize': 10}, fontproperties='monospace')
     plt.axis('off')
-    plt.savefig(f'{output_folder}/logistic_reg_classification_report.png', bbox_inches='tight')
+    plt.savefig(
+        f'{output_folder}/logistic_reg_classification_report.png',
+        bbox_inches='tight')
     plt.close()
 
 
@@ -266,38 +292,40 @@ def roc_curve_plot(y_test: pd.DataFrame,
                    y_test_preds_lr: pd.DataFrame,
                    y_test_preds_rf: pd.DataFrame,
                    output_pth: str) -> None:
-   '''
-   creates roc curve plots for testing set and stores them as png
-   images
-   input:
-        y_test:  test response values
-        y_test_preds_lr: test predictions from logistic regression
-        y_test_preds_rf: test predictions from random forest
-        output_pth: path to store images
-   output:
-        None
-   '''
-   # Get ROC Curves for both models
-   false_positive_lrc, true_positive_lrc, _ = roc_curve(y_test, y_test_preds_lr)
-   false_positive_rfc, true_positive_rfc, _ = roc_curve(y_test, y_test_preds_rf)
-   roc_auc_lrc = roc_auc_score(y_test, y_test_preds_lr)
-   roc_auc_rfc = roc_auc_score(y_test, y_test_preds_rf)
-      
-   # Get Plots
-   _, ax = plt.subplots(figsize=(8,5))
-   ax.plot(false_positive_lrc, true_positive_lrc, color='blue',
-           label=f'Logistic Regression (AUC: {roc_auc_lrc:.2f})')
-   ax.plot(false_positive_rfc, true_positive_rfc, color='green',
-           label=f'Random Forest (AUC: {roc_auc_rfc:.2f})')
-   ax.plot([0, 1], [0, 1], color='red', label='Random (AUC: 0.5)',
-           ls='--')
-   ax.legend(loc='best')
-   ax.set_xlabel('False Positive Ratio', fontsize=12)
-   ax.set_ylabel('True Positive Ratio', fontsize=12)
-   ax.set_title('Testing Set ROC AUC Curves', fontsize=12)
-   plt.savefig(f'{output_pth}/roc_curves.png', bbox_inches='tight')
-   plt.close()
-   
+    '''
+    creates roc curve plots for testing set and stores them as png
+    images
+    input:
+         y_test:  test response values
+         y_test_preds_lr: test predictions from logistic regression
+         y_test_preds_rf: test predictions from random forest
+         output_pth: path to store images
+    output:
+         None
+    '''
+    # Get ROC Curves for both models
+    false_positive_lrc, true_positive_lrc, _ = roc_curve(
+        y_test, y_test_preds_lr)
+    false_positive_rfc, true_positive_rfc, _ = roc_curve(
+        y_test, y_test_preds_rf)
+    roc_auc_lrc = roc_auc_score(y_test, y_test_preds_lr)
+    roc_auc_rfc = roc_auc_score(y_test, y_test_preds_rf)
+
+    # Get Plots
+    _, ax = plt.subplots(figsize=(8, 5))
+    ax.plot(false_positive_lrc, true_positive_lrc, color='blue',
+            label=f'Logistic Regression (AUC: {roc_auc_lrc:.2f})')
+    ax.plot(false_positive_rfc, true_positive_rfc, color='green',
+            label=f'Random Forest (AUC: {roc_auc_rfc:.2f})')
+    ax.plot([0, 1], [0, 1], color='red', label='Random (AUC: 0.5)',
+            ls='--')
+    ax.legend(loc='best')
+    ax.set_xlabel('False Positive Ratio', fontsize=12)
+    ax.set_ylabel('True Positive Ratio', fontsize=12)
+    ax.set_title('Testing Set ROC AUC Curves', fontsize=12)
+    plt.savefig(f'{output_pth}/roc_curves.png', bbox_inches='tight')
+    plt.close()
+
 
 def feature_importance_plot(model_path: str,
                             X_data: pd.DataFrame,
@@ -314,14 +342,14 @@ def feature_importance_plot(model_path: str,
     '''
     # Load model
     rf_model = joblib.load(model_path)
-    
+
     # Compute feature importances
     importances = rf_model.feature_importances_
     indices = np.argsort(importances)[::-1]
     names = [X_data.columns[i] for i in indices]
-    
+
     # Plot Figure
-    plt.figure(figsize=(12,6))
+    plt.figure(figsize=(12, 6))
     plt.title("Feature Importance", fontsize=12)
     plt.ylabel('Importance', fontsize=12)
     plt.bar(range(X_data.shape[1]), importances[indices])
@@ -329,7 +357,6 @@ def feature_importance_plot(model_path: str,
     plt.savefig(f'{output_pth}/feature_importances.png',
                 bbox_inches='tight')
     plt.close()
-    
 
 
 def train_models(X_train: pd.DataFrame,
@@ -352,31 +379,31 @@ def train_models(X_train: pd.DataFrame,
     # Create Models
     rfc = RandomForestClassifier(random_state=SEED)
     lrc = LogisticRegression(solver='lbfgs', max_iter=3000)
-    
+
     # Train Random Forest (with Grid Search) and Get Predictions
     param_grid = {'n_estimators': [200, 500],
                   'max_features': ['auto', 'sqrt'],
-                  'max_depth' : [4, 5, 100],
-                  'criterion' :['gini', 'entropy']
+                  'max_depth': [4, 5, 100],
+                  'criterion': ['gini', 'entropy']
                   }
     cv_rfc = GridSearchCV(estimator=rfc, param_grid=param_grid, cv=5)
     cv_rfc.fit(X_train, y_train)
-    y_train_preds_rf = cv_rfc.best_estimator_.predict_proba(X_train)[:,1]
-    y_test_preds_rf = cv_rfc.best_estimator_.predict_proba(X_test)[:,1]
+    y_train_preds_rf = cv_rfc.best_estimator_.predict_proba(X_train)[:, 1]
+    y_test_preds_rf = cv_rfc.best_estimator_.predict_proba(X_test)[:, 1]
 
     # Train Logistic Regression and Get Predictions
     lrc.fit(X_train, y_train)
-    y_train_preds_lr = lrc.predict_proba(X_train)[:,1]
-    y_test_preds_lr = lrc.predict_proba(X_test)[:,1]
-    
+    y_train_preds_lr = lrc.predict_proba(X_train)[:, 1]
+    y_test_preds_lr = lrc.predict_proba(X_test)[:, 1]
+
     # Write Trained Models as Pickle Files
     best_rfc = cv_rfc.best_estimator_
     joblib.dump(best_rfc, f'{output_folder}/rfc_model.pkl')
     joblib.dump(lrc, f'{output_folder}/logistic_model.pkl')
-    
+
     # Store results
     predictions_lst = y_train_preds_lr, y_train_preds_rf, \
-                        y_test_preds_lr, y_test_preds_rf
+        y_test_preds_lr, y_test_preds_rf
 
     logger.info('Trained Models')
 
@@ -384,46 +411,39 @@ def train_models(X_train: pd.DataFrame,
 
 
 if __name__ == '__main__':
-    
+
     command_line_arguments = sys.argv
-    
+
     if len(command_line_arguments) == 2:
-        
+
         # 1. Load Dataset
         data_path = command_line_arguments[1]
         data = import_data(data_path)
-        
+
         # 2. Perform Exploratory Data Analysis
-        _ = perform_eda(data, 'images/eda')
-        
+        perform_eda(data, 'images/eda')
+
         # 3. Data Preprocessing
         X_train, X_test, y_train, y_test = perform_feature_engineering(
             data, response='Churn'
-            )
+        )
 
         # 4. Train Models
         model_predictions_lst = train_models(X_train, X_test, y_train,
                                              'models')
         y_train_preds_lr, y_train_preds_rf, \
-                y_test_preds_lr, y_test_preds_rf = model_predictions_lst
+            y_test_preds_lr, y_test_preds_rf = model_predictions_lst
 
         # 5. Metrics & Feature Importance
-        _ = classification_report_image(y_train,
-                                        y_test,
-                                        y_train_preds_lr,
-                                        y_train_preds_rf,
-                                        y_test_preds_lr,
-                                        y_test_preds_rf,
-                                        'images/results'
-                                        )
-        _ = roc_curve_plot(y_test,
-                           y_test_preds_lr,
-                           y_test_preds_rf,
-                           'images/results')
-        
-        _ = feature_importance_plot('models/rfc_model.pkl',
-                                    X_test,
-                                    'images/results')
+        classification_report_image(y_train, y_test, y_train_preds_lr,
+                                    y_train_preds_rf, y_test_preds_lr,
+                                    y_test_preds_rf, 'images/results')
+        roc_curve_plot(y_test, y_test_preds_lr, y_test_preds_rf,
+                       'images/results')
+
+        feature_importance_plot('models/rfc_model.pkl', X_test,
+                                'images/results')
 
     else:
-        logger.error('Wrong number of arguments provided. Only requires data path.')
+        logger.error(
+            'Wrong number of arguments provided. Only requires data path.')
--- project_1_customer_churn/churn_script_logging_and_tests.py
+++ project_1_customer_churn/churn_script_logging_and_tests.py
@@ -1,7 +1,9 @@
-# Author - Sebastien
-# Date - 2022/05/24
-# Runs a few assert statements to test functions from customer
-# churn library
+"""
+Author - Sebastien
+Date - 2022/05/24
+
+Runs a few assert statements to test functions from customer churn library
+"""
 
 # Packages
 import os
@@ -24,106 +26,108 @@ logger.addHandler(file_logger)
 
 # Functions
 def test_import(import_data):
-	'''
-	test data import - this example is completed for you to assist with the other test functions
-	'''
-	try:
-		df = import_data("data/bank_data.csv")
-		logger.info("Testing import_data: SUCCESS")
-	except FileNotFoundError as err:
-		logger.error("Testing import_eda: The file wasn't found")
-		raise err
+    '''
+    test data import - this example is completed for you to assist with the other test functions
+    '''
+    try:
+        df = import_data("data/bank_data.csv")
+        logger.info("Testing import_data: SUCCESS")
+    except FileNotFoundError as err:
+        logger.error("Testing import_eda: The file wasn't found")
+        raise err
 
-	try:
-		assert df.shape[0] > 0
-		assert df.shape[1] > 0
-	except AssertionError as err:
-		logger.error("Testing import_data: The file doesn't appear to have rows and columns")
-		raise err
+    try:
+        assert df.shape[0] > 0
+        assert df.shape[1] > 0
+    except AssertionError as err:
+        logger.error(
+            "Testing import_data: The file doesn't appear to have rows and columns")
+        raise err
 
 
 def test_eda(perform_eda, import_data):
-	'''
-	test perform eda function
-	'''
-	try:
-		df = import_data("data/bank_data.csv")
-		_ = perform_eda(df, 'images/eda')
-		nb_image_files = len(os.listdir('images/eda'))
-		assert nb_image_files == 5
-		logger.info('Testing perform_eda: SUCCESS')
-		
-	except AssertionError as err:
-		logger.error('Testing perform_eda: There should be 5 exploratory graphs in folder "images/eda"')
-		raise err
+    '''
+    test perform eda function
+    '''
+    try:
+        df = import_data("data/bank_data.csv")
+        _ = perform_eda(df, 'images/eda')
+        nb_image_files = len(os.listdir('images/eda'))
+        assert nb_image_files == 5
+        logger.info('Testing perform_eda: SUCCESS')
+
+    except AssertionError as err:
+        logger.error(
+            'Testing perform_eda: There should be 5 exploratory graphs in folder "images/eda"')
+        raise err
 
 
 def test_encoder_helper(encoder_helper, import_data):
-	'''
-	test encoder helper
-	'''
-	try:
-		df = import_data('data/bank_data.csv')
-		category_vars = ['Gender', 'Education_Level', 'Marital_Status',
+    '''
+    test encoder helper
+    '''
+    try:
+        df = import_data('data/bank_data.csv')
+        category_vars = ['Gender', 'Education_Level', 'Marital_Status',
                          'Income_Category', 'Card_Category']
-		df_encoded = encoder_helper(df, category_vars, response = 'Churn')
-		category_vars_new = [f'{col}_Churn' for col in category_vars]		
-  
-		# Check if every categorical variable is now encoded
-		# as a float
-		assert all(pandas_types.is_numeric_dtype(df_encoded[col])
-             	   for col in category_vars_new)
-		logger.info('Testing encoder_helper: SUCCESS')
-	
-	except AssertionError as err:
-		logger.error('Testing encoder_helper: Encoding failed. Not all columns are encoded as floats')
-		logger.error(df_encoded[category_vars_new].dtypes)
-		raise err
+        df_encoded = encoder_helper(df, category_vars, response='Churn')
+        category_vars_new = [f'{col}_Churn' for col in category_vars]
+
+        # Check if every categorical variable is now encoded
+        # as a float
+        assert all(pandas_types.is_numeric_dtype(df_encoded[col])
+                   for col in category_vars_new)
+        logger.info('Testing encoder_helper: SUCCESS')
+
+    except AssertionError as err:
+        logger.error(
+            'Testing encoder_helper: Encoding failed. Not all columns are encoded as floats')
+        logger.error(df_encoded[category_vars_new].dtypes)
+        raise err
 
 
 def test_perform_feature_engineering(perform_feature_engineering, import_data):
-	'''
-	test perform_feature_engineering
-	'''
-	try:
-		df = import_data('data/bank_data.csv')
-		X_train, _, _, _ = perform_feature_engineering(df, 'Churn')
-		
-		assert len(X_train.columns) == 19
-		logger.info('Testing perform_feature_engineering: SUCCESS')
+    '''
+    test perform_feature_engineering
+    '''
+    try:
+        df = import_data('data/bank_data.csv')
+        X_train, _, _, _ = perform_feature_engineering(df, 'Churn')
 
-	except AssertionError as err:
-		logger.error('Testing perform_feature_engineering: Failed. There should be 19 features in dataset')
-		raise err
+        assert len(X_train.columns) == 19
+        logger.info('Testing perform_feature_engineering: SUCCESS')
+
+    except AssertionError as err:
+        logger.error(
+            'Testing perform_feature_engineering: Failed. There should be 19 features in dataset')
+        raise err
 
 
 def test_train_models(train_models, perform_feature_engineering, import_data):
-	'''
-	test train_models
-	'''
-	try:
-		df = import_data('data/bank_data.csv')
-		X_train, X_test, y_train, _ = perform_feature_engineering(df, 'Churn')
-		_ = train_models(X_train, X_test, y_train, 'models')
+    '''
+    test train_models
+    '''
+    try:
+        df = import_data('data/bank_data.csv')
+        X_train, X_test, y_train, _ = perform_feature_engineering(df, 'Churn')
+        _ = train_models(X_train, X_test, y_train, 'models')
 
-		# Check if two trained models (Logistic Regression and
-  		# Random Forests) were serialized
-		nb_models_trained = len(os.listdir('models/'))
-		assert nb_models_trained == 2
-		logger.info('Testing train_models: SUCCESS')
+        # Check if two trained models (Logistic Regression and
+        # Random Forests) were serialized
+        nb_models_trained = len(os.listdir('models/'))
+        assert nb_models_trained == 2
+        logger.info('Testing train_models: SUCCESS')
 
-	except AssertionError as err:
-		logger.error('Testing train_models: Failed. There should be 2 serialized models')
-		raise err
+    except AssertionError as err:
+        logger.error(
+            'Testing train_models: Failed. There should be 2 serialized models')
+        raise err
 
 
 if __name__ == "__main__":
-	
-	test_import(import_data)
-	test_eda(perform_eda, import_data)
-	test_encoder_helper(encoder_helper, import_data)
-	test_perform_feature_engineering(perform_feature_engineering, import_data)
-	test_train_models(train_models, perform_feature_engineering, import_data)
 
-
-
+    test_import(import_data)
+    test_eda(perform_eda, import_data)
+    test_encoder_helper(encoder_helper, import_data)
+    test_perform_feature_engineering(perform_feature_engineering, import_data)
+    test_train_models(train_models, perform_feature_engineering, import_data)
